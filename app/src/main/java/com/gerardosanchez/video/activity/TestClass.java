738b899e42b171e1c3f6ef8dfb7bae8d003e4b0b
Gerardo Sánchez Hernández <yerald231ger@live.com>
2018-03-15 12:31:49 -0600

    ok

---
+++ app/src/main/java/com/gerardosanchez/video/activity/TestClass.java
@@ -0,0 +1,23 @@
+package com.gerardosanchez.video.activity;
+
+/**
+ * Created by Gerardo.Sanchez on 15/03/2018.
+ */
+
+public class TestClass {
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
